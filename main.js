829e0d43b3a8e522ac03a9e13e1234a6621d01c2
Dylan Palmboom <nofxboy1234@gmail.com>
2022-08-16 17:12:15 +0200

    Set up starting files and Book Object Constructor

---
+++ main.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    let status = this.read ? 'has been read' : 'not read yet';
+    const info = `${this.title} by ${this.author}, ${this.pages} pages, ${status}`;
+    return info;
+  };
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+console.log(theHobbit.info());
