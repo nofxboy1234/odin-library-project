a1b7db26057b2d013397aba16715e0adb5d91f31
Dylan Palmboom <nofxboy1234@gmail.com>
2022-08-16 21:16:01 +0200

    Loop through library and add to shelf

--- main.js
+++ main.js
@@ -16,38 +16,33 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function addBookToShelf(book) {
-  const bookDiv = document.createElement('div');
-  bookDiv.classList.add('book-card');
+function addLibraryToShelf() {
+  myLibrary.forEach((book) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book-card');
 
-  const bookTitle = document.createElement('div');
-  bookTitle.textContent = book.title;
-  bookDiv.appendChild(bookTitle);
-  const bookAuthor = document.createElement('div');
-  bookAuthor.textContent = book.author;
-  bookDiv.appendChild(bookAuthor);
+    const bookTitle = document.createElement('div');
+    bookTitle.textContent = book.title;
+    bookDiv.appendChild(bookTitle);
+    const bookAuthor = document.createElement('div');
+    bookAuthor.textContent = book.author;
+    bookDiv.appendChild(bookAuthor);
 
-  bookShelf.appendChild(bookDiv);
-}
-
-function showLibrary() {
-  myLibrary.forEach((book) => console.log(book));
+    bookShelf.appendChild(bookDiv);
+  });
 }
 
 const bookShelf = document.querySelector('#book-shelf');
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 addBookToLibrary(theHobbit);
-addBookToShelf(theHobbit);
 
 const theGunslinger = new Book('The Gunslinger', 'Stephen King', 190, true);
 addBookToLibrary(theGunslinger);
-addBookToShelf(theGunslinger);
 
-// addBookToLibrary(theGunslinger);
+addLibraryToShelf();
+
 // const braveNewWorld = new Book('Brave New World', 'Aldous Huxley', 311, true);
 // addBookToLibrary(braveNewWorld);
 // const nineteenEightyFour = new Book('1984', 'George Orwell', 328, true);
 // addBookToLibrary(braveNewWorld);
-
-showLibrary();
