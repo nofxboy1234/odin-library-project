d6be01efa4975d0e0cd9f11500ac5591f46c504e
Dylan Palmboom <nofxboy1234@gmail.com>
2022-08-16 18:12:58 +0200

    Add 1 book to the book shelf grid

--- main.js
+++ main.js
@@ -16,7 +16,32 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
-// console.log(theHobbit.info());
+function showLibrary() {
+  myLibrary.forEach((book) => console.log(book));
+}
+
+const bookShelf = document.querySelector('#book-shelf');
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 addBookToLibrary(theHobbit);
-console.log(myLibrary);
+
+const bookDiv = document.createElement('div');
+bookDiv.classList.add('book-card');
+
+const bookTitle = document.createElement('div');
+bookTitle.textContent = theHobbit.title;
+bookDiv.appendChild(bookTitle);
+const bookAuthor = document.createElement('div');
+bookAuthor.textContent = theHobbit.author;
+bookDiv.appendChild(bookAuthor);
+
+bookShelf.appendChild(bookDiv);
+
+const theGunslinger = new Book('The Gunslinger', 'Stephen King', 190, true);
+addBookToLibrary(theGunslinger);
+const braveNewWorld = new Book('Brave New World', 'Aldous Huxley', 311, true);
+addBookToLibrary(braveNewWorld);
+const nineteenEightyFour = new Book('1984', 'George Orwell', 328, true);
+addBookToLibrary(braveNewWorld);
+
+showLibrary();
