cb20e08239e03a6671aaa1e8ee76854880389c48
Dylan Palmboom <nofxboy1234@gmail.com>
2022-08-18 14:01:14 +0200

    Add modal basic setup

--- main.js
+++ main.js
@@ -47,3 +47,21 @@ const nineteenEightyFour = new Book('1984', 'George Orwell', 328, true);
 addBookToLibrary(nineteenEightyFour);
 
 addLibraryToShelf();
+
+const modal = document.querySelector('.modal');
+const trigger = document.querySelector('.trigger');
+const closeButton = document.querySelector('.close-button');
+
+function toggleModal() {
+  modal.classList.toggle('show-modal');
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
