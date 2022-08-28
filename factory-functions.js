1762e6b72838b73f0811d57c3bb130e83d22a528
Dylan Palmboom <nofxboy1234@gmail.com>
2022-08-28 12:59:28 +0200

    Add more constructor function tests

--- factory-functions.js
+++ factory-functions.js
@@ -17,18 +17,57 @@
 // console.log(c);
 // console.log(window.instance_member); // Property added to global namespace!
 
-function C() {
-  console.log('start of constructor');
-  console.log(this);
-  if (!(this instanceof C)) {
-    console.log('return new C()');
-    return new C();
-  }
-  this.instance_member = 'This is ok.';
-  console.log('end of constructor');
-}
-var c = C(); // Is this a constructor?
-console.log(c);
-console.log(window.instance_member); // No global namespace pollution.
-console.log(c instanceof C); // It's like a class, right?
-console.log(c.constructor === C);
+// function C() {
+//   console.log('start of constructor');
+//   console.log(this);
+//   if (!(this instanceof C)) {
+//     console.log('return new C()');
+//     return new C();
+//   }
+//   this.instance_member = 'This is ok.';
+//   console.log('end of constructor');
+// }
+// var c = C(); // Is this a constructor?
+// console.log(c);
+// console.log(window.instance_member); // No global namespace pollution.
+// console.log(c instanceof C); // It's like a class, right?
+// console.log(c.constructor === C);
+
+// Constructor
+// function C() {}
+// // Create object.
+// var c = new C();
+// console.log(c instanceof C);
+// console.log(c.constructor === C);
+// // Change prototype
+// C.prototype = { prototype_prop: 'proto' };
+// console.log(c.constructor === C);
+// console.log(c instanceof C); // instanceof no longer works!
+
+// Create two constructors with the
+// same prototype.
+// var proto = { protoprop: 'protoprop' };
+// function C() {
+//   this.cprop = 'cprop';
+// }
+// C.prototype = proto;
+// function F() {
+//   this.fprop = 'fprop';
+// }
+// F.prototype = proto;
+// var f = new F();
+// console.log(f.protoprop); // Has prototype properties
+// console.log(f.fprop); // Has F properties
+// console.log(f.cprop); // Doesn't have C properties
+// console.log(f instanceof C); // Is an instance of C!?!
+// console.log(F.prototype.isPrototypeOf(f));
+
+function C() {}
+C.prototype = { prototype_prop: 'proto' };
+// Changing the prototype breaks the constructor
+// property for all objects created after the change.
+c = new C();
+console.log(c instanceof C); //=> true
+console.log(c.constructor === C); //=> false
+C.prototype.constructor = C;
+console.log(c.constructor === C); //=> true
